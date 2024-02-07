65830c33265261ed3749ca8341eb08a3d3b5147c
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-02-06 17:20:09 -0800

    javascript

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const topics = ['Html', 'CSS', 'Git', 'JavaScript'];
+for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!"); 
+} else if (topic === 'Git') {
+    console.lot("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
